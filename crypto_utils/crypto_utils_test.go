f025acf17188ba8c66c5d058145bccaf8abbef27
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-03 10:26:05 -0800

    added test case

--- crypto_utils/crypto_utils_test.go
+++ crypto_utils/crypto_utils_test.go
@@ -2,8 +2,12 @@ package crypto_utils
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetMd5(t *testing.T) {
-
+	result := GetMd5("abc123")
+	assert.NotNil(t, result)
+	assert.EqualValues(t, "e99a18c428cb38d5f260853678922e03", result)
 }
