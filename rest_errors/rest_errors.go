ad09a0b3cfadf40bcc48c856ea5eddc0cdbca908
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-07 09:50:45 -0800

    updated the error lib to export an interface instad of sturct

--- rest_errors/rest_errors.go
+++ rest_errors/rest_errors.go
@@ -1,56 +1,92 @@
 package rest_errors
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 const (
 	// DatabaseErrorMsg string
 	DatabaseErrorMsg = "database error"
 )
 
-// RestErr a unified common error struct
-type RestErr struct {
-	Message string        `json:"message"`
-	Status  int           `json:"status"`
-	Error   string        `json:"error"`
-	Causes  []interface{} `json:"causes"`
+// RestErr the restErr interface
+type RestErr interface {
+	Message() string
+	Status() int
+	Error() string
+	Causes() []interface{}
+}
+
+type restErr struct {
+	message string        `json:"message"`
+	status  int           `json:"status"`
+	error   string        `json:"error"`
+	causes  []interface{} `json:"causes"`
+}
+
+func (e *restErr) Message() string {
+	return e.message
+}
+
+func (e *restErr) Status() int {
+	return e.status
+}
+
+func (e *restErr) Error() string {
+	return fmt.Sprintf("message: %s - status: %d - error: %s - causes: [ %v ]", e.message, e.status, e.error, e.causes)
+}
+
+func (e *restErr) Causes() []interface{} {
+	return e.causes
+}
+
+// NewRestError returns a custom RestError
+func NewRestError(message string, status int, err string, causes []interface{}) RestErr {
+	return &restErr{
+		message: message,
+		status:  status,
+		error:   err,
+		causes:  causes,
+	}
 }
 
 // NewBadRequestError returns a RestErr for bad request errors
-func NewBadRequestError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusBadRequest,
-		Error:   "bad_request",
+func NewBadRequestError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status:  http.StatusBadRequest,
+		error:   "bad_request",
 	}
 }
 
 // NewNotFoundError returns a RestErr for not found errors
-func NewNotFoundError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusNotFound,
-		Error:   "not_found",
+func NewNotFoundError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status:  http.StatusNotFound,
+		error:   "not_found",
 	}
 }
 
 // NewInternalServerError returns a RestErr for internal server error
-func NewInternalServerError(message string, err error) *RestErr {
-	restErr := &RestErr{
-		Message: message,
-		Status:  http.StatusInternalServerError,
-		Error:   "internal_server_error",
+func NewInternalServerError(message string, err error) RestErr {
+	restErr := &restErr{
+		message: message,
+		status:  http.StatusInternalServerError,
+		error:   "internal_server_error",
 	}
 	if err != nil {
-		restErr.Causes = append(restErr.Causes, err.Error())
+		restErr.causes = append(restErr.causes, err.Error())
 	}
 	return restErr
 }
 
 // NewUnAuthorizedError returns a RestError for unauthorized error
-func NewUnAuthorizedError(message string) *RestErr {
-	return &RestErr{
-		Message: message,
-		Status:  http.StatusUnauthorized,
-		Error:   "unauthorized",
+func NewUnAuthorizedError(message string) RestErr {
+	return &restErr{
+		message: message,
+		status:  http.StatusUnauthorized,
+		error:   "unauthorized",
 	}
 }
--- rest_errors/rest_errors_test.go
+++ rest_errors/rest_errors_test.go
@@ -10,34 +10,30 @@ import (
 func TestNewInternalServerError(t *testing.T) {
 	err := NewInternalServerError("test message", errors.New("database error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "test message", err.Message)
-	assert.EqualValues(t, 500, err.Status)
-	assert.EqualValues(t, "internal_server_error", err.Error)
+	assert.EqualValues(t, "test message", err.Message())
+	assert.EqualValues(t, 500, err.Status())
 
-	assert.NotNil(t, 1, len(err.Causes))
-	assert.EqualValues(t, "database error", err.Causes[0])
+	assert.NotNil(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "database error", err.Causes()[0])
 }
 
 func TestNewBadRequestError(t *testing.T) {
 	err := NewBadRequestError("test message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "test message", err.Message)
-	assert.EqualValues(t, 400, err.Status)
-	assert.EqualValues(t, "bad_request", err.Error)
+	assert.EqualValues(t, "test message", err.Message())
+	assert.EqualValues(t, 400, err.Status())
 }
 
 func TestNewNotFoundError(t *testing.T) {
 	err := NewNotFoundError("test message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "test message", err.Message)
-	assert.EqualValues(t, 404, err.Status)
-	assert.EqualValues(t, "not_found", err.Error)
+	assert.EqualValues(t, "test message", err.Message())
+	assert.EqualValues(t, 404, err.Status())
 }
 
 func TestNewUnAuthorizedError(t *testing.T) {
 	err := NewUnAuthorizedError("test message")
 	assert.NotNil(t, err)
-	assert.EqualValues(t, "test message", err.Message)
-	assert.EqualValues(t, 401, err.Status)
-	assert.EqualValues(t, "unauthorized", err.Error)
+	assert.EqualValues(t, "test message", err.Message())
+	assert.EqualValues(t, 401, err.Status())
 }
