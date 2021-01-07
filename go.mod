5d2dffa76567ab31a6db123425dc07e3c399a9d5
Ahmed Soliman <ahmed.soliman@programmer.net>
2021-01-07 06:43:35 -0800

    added Logger lib to the shard util lib

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/a-soliman/bookstore_utils-go
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/stretchr/testify v1.6.1
+	go.uber.org/zap v1.16.0
+)
---
+++ logger/logger.go
@@ -0,0 +1,66 @@
+package logger
+
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	// Log writes a logger line
+	log Logger
+)
+
+// Logger the logger struct
+type Logger struct {
+	log *zap.Logger
+}
+
+func init() {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:     "level",
+			TimeKey:      "time",
+			MessageKey:   "msg",
+			EncodeTime:   zapcore.ISO8601TimeEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
+			EncodeCaller: zapcore.ShortCallerEncoder,
+		},
+	}
+
+	var err error
+	if log.log, err = logConfig.Build(); err != nil {
+		panic(err)
+	}
+}
+
+// Printf returns info log
+func (l Logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
+// GetLogger returns the logger instance
+func GetLogger() Logger {
+	return log
+}
+
+// Info logs an info level
+func Info(msg string, tags ...zap.Field) {
+	log.log.Info(msg, tags...)
+	log.log.Sync()
+}
+
+// Error logs an error level
+func Error(msg string, err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.log.Error(msg, tags...)
+	log.log.Sync()
+}
